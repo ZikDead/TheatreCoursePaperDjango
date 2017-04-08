263c18a2ad2f8d571ea6be1cae651389e7a49b1a
ZikDead <vladziku@gmail.com>
2017-04-08 19:37:46 +0300

    Added registration section

--- HtmlCoding/app/js/common.js
+++ HtmlCoding/app/js/common.js
@@ -1,33 +1,47 @@
 $(function() {
 
-
   // Slider of schedule
-  $('.menu').slick({
+  (function() {
+    $('.menu').slick({
 
-  infinite: false,
-  centerPadding: '0px',
-  slidesToShow: 4,
-  prevArrow: $('.left-pagination'),
-  nextArrow: $('.right-pagination'),
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerPadding: '40px',
-        slidesToShow: 3
+    infinite: false,
+    centerPadding: '0px',
+    slidesToShow: 4,
+    prevArrow: $('.left-pagination'),
+    nextArrow: $('.right-pagination'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerPadding: '40px',
-        slidesToShow: 2
-      }
-    }
-  ]
-});
+    ]
+    });
+  })();
+
+  // Log in animation
+  (function () {
+    $('#log-in').click(function () {
+        $(this).blur();
+        $('.log-in').animate({
+        height: "toggle"
+        }, 500);
+    })
+  })();
+
+
+
 
 
 });
