40c10438069a2340f1dfbb65ebcae6557e5be868
ZikDead <vladziku@gmail.com>
2017-04-13 15:21:41 +0300

    Rebuild of models

--- TheatreDjangoApp/theatre/models.py
+++ TheatreDjangoApp/theatre/models.py
@@ -1,37 +1,41 @@
 from django.db import models
 
 
-
 class Hall(models.Model):
+    number_of_hall = models.IntegerField()
     number_of_places = models.IntegerField()
-    description_of_hall = models.CharField(max_length=100)
+    # JSON format
     place = models.TextField(default='')
-    def __unicode__(self):
+
+    def __str__(self):
         return self.id
+
     class Meta:
         verbose_name_plural = 'Halls'
 
 
 class Perfomance(models.Model):
-    cost_of_ticket = models.FloatField()
-    tittle= models.CharField(max_length=70)
+    cost_of_ticket = models.DecimalField(decimal_places=2)
+    tittle = models.CharField(max_length=70)
 
 
 class Program(models.Model):
-    time = models.TimeField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
     data = models.DateField()
+
     perfomance = models.ForeignKey(Perfomance)
-    hall = models.ForeignKey(Hall)
-    places = models.CharField(max_length=2000)
-    #JSON format
-    #object = ProgrammManager
-    def __unicode__(self):
+    hall = models.OneToOneField(Hall)
+
+    def __str__(self):
         return self.id
-    # def get_absolute_url(self):
-    #     return '/program/%d' % self.id
+
     class Meta:
         ordering = ['-time']
+        verbose_name_plural = 'Program'
 
 # class ProgrammtManager(models.Manager):
 #     def last_sessions(self):
 #         return self.filter(time__gt<fwefw)
+# def get_absolute_url(self):
+#     return '/program/%d' % self.id
